b05e8a88bb5839b5b497289ef278ae0b44c0b933
Tamer Tamer <ttamer@ucsb.edu>
2022-10-09 15:05:53 -0700

    st[4

--- fibvec/FibVec.cpp
+++ fibvec/FibVec.cpp
@@ -5,8 +5,29 @@
 using std::cout;
 
 FibVec::FibVec(){
-    point= new int[1];
+    point= new int[1]
 }
 FibVec::~FibVec(){
     delete [] point;
+}
+size_t FibVec::capacity() const{
+ 
+}
+size_t FibVec::count() const{
+    
+}
+void FibVec::insert(int value, size_t index){
+    
+}
+int FibVec::lookup(size_t index) const{
+    
+}
+void FibVec::pop(){
+    
+}
+void FibVec::push(int value){
+    
+}
+void FibVec::remove(size_t index){
+    
 }
