afcf87f68411f3f5339255db821428893a2c9f96
Tamer Tamer <ttamer@ucsb.edu>
2022-10-09 14:45:33 -0700

    stp3

---
+++ fibvec/FibVec.cpp
@@ -0,0 +1,15 @@
+#include "FibVec.h"
+#include <stdexcept>
+#include <iostream>
+
+using std::cout;
+
+FibVec::FibVec(){
+    point= new int[1]
+}
+FibVec::~FibVec(){
+    delete [] point;
+}
+size_t FibVec::capacity() const{
+    
+}
