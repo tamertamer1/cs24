e7fa788ad361c68a086ac4476fc4b865e61e3fd1
Tamer Tamer <ttamer@ucsb.edu>
2022-10-16 19:54:13 -0700

    lol

--- assort/List.cpp
+++ assort/List.cpp
@@ -131,6 +131,7 @@ std::string List::remove(size_t index){
         if (index==0){
             std::string c=cur->data;
             head=cur->next;
+            free(cur)
             return c;
         }
         while (l+1!=index){
@@ -140,10 +141,12 @@ std::string List::remove(size_t index){
         if(!cur->next->next){
             std::string c=cur->next->data;
             cur->next=NULL;
+            free(cur)
             return c;
         }
         std::string c =cur->next->data;
         cur->next=cur->next->next;
+        free(cur)
         return c;
     }
 }    
