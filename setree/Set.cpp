95f6ac74dc91866485b2e373ec634a0aa48d6097
Tamer Tamer <ttamer@ucsb.edu>
2022-10-23 21:54:30 -0700

    lol

--- setree/Set.cpp
+++ setree/Set.cpp
@@ -60,9 +60,11 @@ void Set::print() const{
 
 size_t Set::remove(const std::string& value){
     size_t inct=count();
-    mRoot=delete_node(mRoot,value);
+    delete_node(mRoot,value);
     if (count()!=inct){
         return 1;
     }
     return 0;
 };
+
+
