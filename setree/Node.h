732fdd96a05bdfb2d4624e0394d994a080791ec9
Tamer Tamer <ttamer@ucsb.edu>
2022-10-23 22:39:27 -0700

    l

--- setree/Node.h
+++ setree/Node.h
@@ -107,115 +107,46 @@ Node* copyRec(Node *in){
     sec->left=copyRec(in->left);
     sec->right=copyRec(in->right);
     return sec;
-};
+}
 
-Node* getMinimumKey(Node* curr)
+Node* delete_node(Node *root, std::string data)
 {
-    while (curr->left != nullptr) {
-        curr = curr->left;
-    }
-    return curr;
-};
+  if(root == nullptr) return root;
+  else if(data < root->data) root->left  = delete_node(root->left, data);
+  else if(data > root->data) root->right = delete_node(root->right, data);
 
-void searchKey(Node* &curr, std::string key, Node* &parent)
-{
-    // traverse the tree and search for the key
-    while (curr != nullptr && curr->data != key)
+  else
+  {
+    if(root->left == nullptr && root->right == nullptr) // Case 1
     {
-        // update the parent to the current node
-        parent = curr;
- 
-        // if the given key is less than the current node, go to the left subtree;
-        // otherwise, go to the right subtree
-        if (key < curr->data) {
-            curr = curr->left;
-        }
-        else {
-            curr = curr->right;
-        }
+      delete root;  
+      root = nullptr;
     }
-};
+    else if(root->left == nullptr)       // Case 2
+    {
+       Node* temp = root;
+       root= root->right;
+       delete temp;
+       temp=nullptr;
+    }
+    else if(root->right == nullptr)      // Case 2
+    {
+       Node* temp = root;
+       root = root->left;
+       delete temp;
+       temp=nullptr;
+    }
+    else                                 // Case 3
+    {
+       Node* temp = root->right;
 
-void deleteNode(Node*& root, std::string key)
-{
-    // pointer to store the parent of the current node
-    Node* parent = nullptr;
- 
-    // start with the root node
-    Node* curr = root;
- 
-    // search key in the BST and set its parent pointer
-    searchKey(curr, key, parent);
- 
-    // return if the key is not found in the tree
-    if (curr == nullptr) {
-        return;
+       while(temp->left != nullptr) temp = temp->left;
+
+       root->data = temp->data;
+       root->right = delete_node(root->right, temp->data);
+
     }
- 
-    // Case 1: node to be deleted has no children, i.e., it is a leaf node
-    if (curr->left == nullptr && curr->right == nullptr)
-    {
-        // if the node to be deleted is not a root node, then set its
-        // parent left/right child to null
-        if (curr != root)
-        {
-            if (parent->left == curr) {
-                parent->left = nullptr;
-            }
-            else {
-                parent->right = nullptr;
-            }
-        }
-        // if the tree has only a root node, set it to null
-        else {
-            root = nullptr;
-        }
- 
-        // deallocate the memory
-        delete curr;        // or delete curr;
-    }
- 
-    // Case 2: node to be deleted has two children
-    else if (curr->left && curr->right)
-    {
-        // find its inorder successor node
-        Node* successor = getMinimumKey(curr->right);
- 
-        // store successor value
-        std::string val = successor->data;
- 
-        // recursively delete the successor. Note that the successor
-        // will have at most one child (right child)
-        deleteNode(root, successor->data);
- 
-        // copy value of the successor to the current node
-        curr->data = val;
-    }
- 
-    // Case 3: node to be deleted has only one child
-    else {
-        // choose a child node
-        Node* child = (curr->left)? curr->left: curr->right;
- 
-        // if the node to be deleted is not a root node, set its parent
-        // to its child
-        if (curr != root)
-        {
-            if (curr == parent->left) {
-                parent->left = child;
-            }
-            else {
-                parent->right = child;
-            }
-        }
- 
-        // if the node to be deleted is a root node, then set the root to the child
-        else {
-            root = child;
-        }
- 
-        // deallocate the memory
-        delete curr;
-    }
-};
+  }
+  return root;
+}
 
