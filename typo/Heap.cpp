5edaf05fc53a06d145a33efcb29e9498a619999b
Tamer Tamer <ttamer@ucsb.edu>
2022-11-11 12:45:57 -0800

    heap

--- typo/Heap.cpp
+++ typo/Heap.cpp
@@ -71,18 +71,28 @@ Heap::Entry Heap::pushpop(const std::string& value, float score){
         mData[minscoreind]=tempent;
         size_t currind=minscoreind;
 
-        while((((currind*2+1)<mCount) && (mData[currind*2+1].score<mData[currind].score))||(((currind*2+2)<mCount) && (mData[currind*2+2].score<mData[currind].score))){
-            if( mData[currind*2+1].score<= mData[currind*2+2].score){
-                Entry tempent=mData[currind*2+1];
-                mData[currind*2+1]=mData[currind];
-                mData[currind]=tempent;
-                currind=currind*2+1;
+        while(((currind*2+1)<mCount)||(((currind*2+2)<mCount))){
+            if((currind*2+2)<mCount){
+                if( mData[currind*2+1].score<= mData[currind*2+2].score){
+                    Entry tempent=mData[currind*2+1];
+                    mData[currind*2+1]=mData[currind];
+                    mData[currind]=tempent;
+                    currind=currind*2+1;
+                }
+                else{
+                    Entry tempent=mData[currind*2+2];
+                    mData[currind*2+2]=mData[currind];
+                    mData[currind]=tempent;
+                    currind=currind*2+2;
+                }
             }
             else{
-                Entry tempent=mData[currind*2+2];
-                mData[currind*2+2]=mData[currind];
-                mData[currind]=tempent;
-                currind=currind*2+2;
+                if(mData[currind*2+1].score<mData[currind].score){
+                    Entry tempent=mData[currind*2+1];
+                    mData[currind*2+1]=mData[currind];
+                    mData[currind]=tempent;
+                    currind=currind*2+1;
+                }
             }
         }
         return minent;
